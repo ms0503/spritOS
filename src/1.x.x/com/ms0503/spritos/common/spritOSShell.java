aafd4f5d2ae9d9a002ed11d845b5053484657208
ms0503 <ms0503@outlook.com>
2019-11-30 10:09:48 +0900

    1.1.0 公開。

--- src/1.x.x/com/ms0503/spritos/common/spritOSShell.java
+++ src/1.x.x/com/ms0503/spritos/common/spritOSShell.java
@@ -7,7 +7,7 @@ import com.ms0503.spritos.server.core.spritOSPackageManager;
 import java.io.IOException;
 
 public class spritOSShell {
-    public static void main(String[] args) throws Exception {
+    public void run() {
         String[] cmd = new String[0];
         String cd = "/user";
         System.out.println("#################");
@@ -15,18 +15,36 @@ public class spritOSShell {
         System.out.println("#   Ver.1.0.0   #");
         System.out.println("#################");
         System.out.println("");
-        spritOSCore.program();
+        try {
+            spritOSCore.program();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
         while(true) {
             for(int i = 0; cmd != null; i++) {
                 cmd[i] = Input.getString(cd);
             }
             if(cmd[0] == "spam") {
                 if(cmd[1] == "install") {
-                    spritOSPackageManager.spamInstall(cmd);
+                    try {
+                        spritOSPackageManager.spamInstall(cmd);
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
                 } else if(cmd[1] == "uninstall") {
-                    spritOSPackageManager.spamUninstall(cmd);
+                    try {
+                        spritOSPackageManager.spamUninstall(cmd);
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
+                } else if(cmd[1] == "upload") {
+                    try {
+                        spritOSPackageManager.spamUpload(cmd[2], cmd[3]);
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
                 } else {
-                    continue;
+                    ;
                 }
             } else if(cmd[0] != null) {
                 try {
@@ -39,11 +57,8 @@ public class spritOSShell {
                         rt.exec("java -jar \"~/spritOS/bin/" + cmd + ".spr\"");
                     } catch(IOException ex) {
                         ex.printStackTrace();
-                        continue;
                     }
                 }
-            } else {
-                continue;
             }
         }
     }
